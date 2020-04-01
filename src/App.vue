48a4aa9b11c3763c53293b2e5889f1dd7f933243
Youssef Touggani <yousseftouggani@MacBook-Pro-de-Youssef.local>
2020-04-01 14:46:33 +0200

    creation  vue menu barre

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <menu-barre/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MenuBarre from './components/MenuBarre.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MenuBarre
+  }, 
+  data: () => {
+    return {
+      
+    }
+  },
+  methods: {
+    
   }
 }
 </script>
@@ -25,4 +31,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/MenuBarre.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="menu-barre">
+        <ul id="Navigation">
+            <li><a href="menu_definir.htm">Exemple 1</a></li>
+            <li><a href="menu_formater.htm">Exemple 2</a></li>
+            <li><a href="menu_rollover.htm">Exemple 3</a></li>
+  </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }, 
+  data: () => {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu-barre{
+    background-color: black;
+    width: 6%;
+    height: 100%;
+    min-height: 100vh;
+    top: 100px;
+}
+ul#menu {
+    margin: 0; padding: 0;
+}
+ul{
+    list-style-type: none;
+}
+ul#menu li {
+    list-style: none;
+    margin: 0; padding: 0;
+    list-style-type: none;
+}
+
+
+</style>
