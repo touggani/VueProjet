69bf0c7508f67dba0bb833d7f28217d7b562bdfe
Florian <florian.asencio@lacatholille.fr>
2020-04-18 16:26:45 +0200

    180420201626

---
+++ src/components/Accueil.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="accueil">
+        <div class="carousel">
+            <img v-bind:src="list[imageIndex]">
+        </div>
+        <button v-on:click="getPreviewImage()">Precedent</button>
+        <button v-on:click="getNextImage()">Suivant</button>
+    </div>
+</template>
+
+<script>
+import laligneverte from '../assets/laligneverte.jpg'
+import meninblack from '../assets/meninblack.jpg'
+import ingloriousbasterds from '../assets/ingloriousbasterds.jpg'
+
+
+export default {
+  name: 'accueil',
+  components: {
+  }, 
+  data: () => {
+    return {
+      id: 0,
+      detail: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
